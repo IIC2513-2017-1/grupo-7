c94db9e92b7bcebf6e7ec97c1390eceea329eb08
Christian Ackermann <caackermann@uc.cl>
2017-04-30 18:36:25 -0300

    signup css

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,15 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-  # GET /users
-  # GET /users.json
+
   def index
     @users = User.all
   end
-
-  # GET /users/1
-  # GET /users/1.json
+  
   def show
     @user = User.find(params[:id])
   end
 
-  # GET /users/new
   def new
     @user = User.new
   end

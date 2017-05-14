5aca0c91769b72a8d009b62d813d4cd481a5f578
Christian Ackermann <caackermann@uc.cl>
2017-05-13 21:43:13 -0300

    finished sessions

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/signup',  to: 'users#new'
-  get  '/login',   to: 'static_pages#login'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
